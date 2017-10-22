e57d659e7e065de0073d0dda9627e0f1a87b6ca3
admin <dimoon.lda@gmail.com>
2017-10-22 23:31:36 +0300

    QC-1: create templates

---
+++ frontend/rest-frontend/src/main/java/ua/kiev/dimoon/questcreator/rest/frontend/configuration/MvcConfig.java
@@ -0,0 +1,21 @@
+package ua.kiev.dimoon.questcreator.rest.frontend.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by admin on 21.10.2017.
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/profile").setViewName("profile");
+        registry.addViewController("/doQuest").setViewName("doQuest");
+    }
+}
